da769e7b161e42bb85560b8fa66410071377afbb
Michal Ostruszka <michal.ostruszka@gmail.com>
2012-07-04 21:32:59 +0200

    few more integration tests

--- src/test/java/pl/project13/maven/git/GitCommitIdMojoIntegrationTest.java
+++ src/test/java/pl/project13/maven/git/GitCommitIdMojoIntegrationTest.java
@@ -4,42 +4,44 @@ import static org.fest.assertions.Assertions.assertThat;
 import static org.mockito.internal.util.reflection.Whitebox.setInternalState;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 
+import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.project.MavenProject;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import pl.project13.maven.git.TemporaryMavenProject.CleanUp;
+import pl.project13.maven.git.FileSystemMavenSandbox.CleanUp;
 
 public class GitCommitIdMojoIntegrationTest {
 
 	private GitCommitIdMojo mojo;
-	private TemporaryMavenProject temporaryProject;
+	private FileSystemMavenSandbox mavenSandbox;
 
 	@Before
 	public void setUp() {
-		temporaryProject = new TemporaryMavenProject();
+		mavenSandbox = new FileSystemMavenSandbox("target/sandbox");
 		mojo = new GitCommitIdMojo();
 		initializeWithDefaults(mojo);
 	}
 	
 	@After
-	public void tearDown() throws Exception {
-		temporaryProject.cleanUp();
+	public void cleanUp() throws IOException {
+		mavenSandbox.cleanup();
 	}
 	
 	@Test
-	public void shouldResolveGitRelatedPropertiesOnDefaultSettingsForJarProject() throws Exception {
-		temporaryProject.withParentProject("target/sandbox/my-jar-project", "jar")
+	public void shouldResolvePropertiesOnDefaultSettingsForNonPomProject() throws Exception {
+		mavenSandbox.withParentProject("my-jar-project", "jar")
 			.withNoChildProject()
 			.withGitRepoInParent()
 			.create(CleanUp.CLEANUP_FIRST);
-		MavenProject targetProject = temporaryProject.getParentProject();
-		setProjectToExecuteIn(targetProject);
+		MavenProject targetProject = mavenSandbox.getParentProject();
+		setProjectContextForMojo(targetProject);
 		
 		// when
 		mojo.execute();
@@ -58,12 +60,119 @@ public class GitCommitIdMojoIntegrationTest {
 	    assertThat(properties).satisfies(new ContainsKeyCondition("git.commit.message.short"));
 	    assertThat(properties).satisfies(new ContainsKeyCondition("git.commit.time"));
 	}
-
+	
+	@Test
+	public void shouldNotRunWhenPackagingPomAndDefaultSettingsApply() throws Exception {
+		// given
+		mavenSandbox.withParentProject("my-pom-project", "pom")
+			.withNoChildProject()
+			.withGitRepoInParent()
+			.create(CleanUp.CLEANUP_FIRST);
+		MavenProject targetProject = mavenSandbox.getParentProject();
+		setProjectContextForMojo(targetProject);
+		
+		// when
+		mojo.execute();
+		
+		// then
+		assertThat(targetProject.getProperties()).isEmpty();
+	}
+	
+	@Test
+	public void shouldRunWhenPackagingPomAndSkipPomsFalse() throws Exception {
+		// given
+		mavenSandbox.withParentProject("my-pom-project", "pom")
+			.withNoChildProject()
+			.withGitRepoInParent()
+			.create(CleanUp.CLEANUP_FIRST);
+		MavenProject targetProject = mavenSandbox.getParentProject();
+		setProjectContextForMojo(targetProject);
+		alterSettings("skipPoms", false);
+		
+		// when
+		mojo.execute();
+		
+		// then
+		assertThat(targetProject.getProperties()).isNotEmpty();		
+	}
+	
+	@Test
+	public void shouldUseParentProjectRepoWhenInvokedFromChild() throws Exception {
+		// given
+		mavenSandbox.withParentProject("my-pom-project", "pom")
+			.withChildProject("my-jar-module", "jar")
+			.withGitRepoInParent()
+			.create(CleanUp.CLEANUP_FIRST);
+		MavenProject targetProject = mavenSandbox.getChildProject();
+		setProjectContextForMojo(targetProject);
+		alterSettings("skipPoms", false);
+		
+		// when
+		mojo.execute();
+		
+		// then
+		Properties properties = targetProject.getProperties();
+	    assertThat(properties).satisfies(new ContainsKeyCondition("git.branch"));
+	    assertThat(properties).satisfies(new ContainsKeyCondition("git.commit.id"));
+	    assertThat(properties).satisfies(new ContainsKeyCondition("git.commit.id.abbrev"));
+	    assertThat(properties).satisfies(new ContainsKeyCondition("git.build.user.name"));
+	    assertThat(properties).satisfies(new ContainsKeyCondition("git.build.user.email"));
+	    assertThat(properties).satisfies(new ContainsKeyCondition("git.commit.user.name"));
+	    assertThat(properties).satisfies(new ContainsKeyCondition("git.commit.user.email"));
+	    assertThat(properties).satisfies(new ContainsKeyCondition("git.commit.message.full"));
+	    assertThat(properties).satisfies(new ContainsKeyCondition("git.commit.message.short"));
+	    assertThat(properties).satisfies(new ContainsKeyCondition("git.commit.time"));
+	}
+	
+	@Test
+	public void shouldUseChildProjectRepoIfInvokedFromChild() throws Exception {
+		// given
+		mavenSandbox.withParentProject("my-pom-project", "pom")
+			.withChildProject("my-jar-module", "jar")
+			.withGitRepoInChild()
+			.create(CleanUp.CLEANUP_FIRST);
+		MavenProject targetProject = mavenSandbox.getChildProject();
+		setProjectContextForMojo(targetProject);
+		alterSettings("skipPoms", false);
+		
+		// when
+		mojo.execute();
+		
+		// then
+		Properties properties = targetProject.getProperties();
+	    assertThat(properties).satisfies(new ContainsKeyCondition("git.branch"));
+	    assertThat(properties).satisfies(new ContainsKeyCondition("git.commit.id"));
+	    assertThat(properties).satisfies(new ContainsKeyCondition("git.commit.id.abbrev"));
+	    assertThat(properties).satisfies(new ContainsKeyCondition("git.build.user.name"));
+	    assertThat(properties).satisfies(new ContainsKeyCondition("git.build.user.email"));
+	    assertThat(properties).satisfies(new ContainsKeyCondition("git.commit.user.name"));
+	    assertThat(properties).satisfies(new ContainsKeyCondition("git.commit.user.email"));
+	    assertThat(properties).satisfies(new ContainsKeyCondition("git.commit.message.full"));
+	    assertThat(properties).satisfies(new ContainsKeyCondition("git.commit.message.short"));
+	    assertThat(properties).satisfies(new ContainsKeyCondition("git.commit.time"));
+	}
+	
+	@Test(expected=MojoExecutionException.class)
+	public void shouldFailWithExceptionWhenNoGitRepoFound() throws Exception {
+		// given
+		mavenSandbox.withParentProject("my-pom-project", "pom")
+			.withChildProject("my-jar-module", "jar")
+			.withNoGitRepoAvailable()
+			.create(CleanUp.CLEANUP_FIRST);
+		MavenProject targetProject = mavenSandbox.getChildProject();
+		setProjectContextForMojo(targetProject);
+		alterSettings("skipPoms", false);
+		
+		// when
+		mojo.execute();
+	}
+	
+	
 	private void alterSettings(String parameterName, Object parameterValue) {
 		setInternalState(mojo, parameterName, parameterValue);
 	}
 	
-	private void setProjectToExecuteIn(MavenProject project) {
+	private void setProjectContextForMojo(MavenProject project) {
 		setInternalState(mojo, "project", project);
 		setInternalState(mojo, "dotGitDirectory", new File(project.getBasedir(), ".git"));		
 	}
