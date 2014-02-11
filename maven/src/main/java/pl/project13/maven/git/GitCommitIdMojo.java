d513b490d79dd48558c8168d3bc3b69aca88c826
jbellmann <joerg.bellmann@googlemail.com>
2014-02-11 09:25:00 +0100

    remove commented code in mojo

--- maven/src/main/java/pl/project13/maven/git/GitCommitIdMojo.java
+++ maven/src/main/java/pl/project13/maven/git/GitCommitIdMojo.java
@@ -17,16 +17,11 @@
 
 package pl.project13.maven.git;
 
-import static com.google.common.base.Strings.isNullOrEmpty;
-
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.text.SimpleDateFormat;
 import java.util.Collections;
-import java.util.Date;
 import java.util.List;
-import java.util.Map;
 import java.util.Properties;
 
 import javax.annotation.Nonnull;
@@ -35,28 +30,12 @@ import javax.annotation.Nullable;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.project.MavenProject;
-import org.eclipse.jgit.api.errors.GitAPIException;
-import org.eclipse.jgit.lib.AbbreviatedObjectId;
-import org.eclipse.jgit.lib.Constants;
-import org.eclipse.jgit.lib.ObjectReader;
-import org.eclipse.jgit.lib.Ref;
-import org.eclipse.jgit.lib.Repository;
-import org.eclipse.jgit.revwalk.RevCommit;
-import org.eclipse.jgit.revwalk.RevWalk;
-import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
 
-import pl.project13.jgit.DescribeCommand;
-import pl.project13.jgit.DescribeResult;
 import pl.project13.maven.git.GitDataLoader.GitDataLoaderBuilder;
 import pl.project13.maven.git.log.LoggerBridge;
 import pl.project13.maven.git.log.MavenLoggerBridge;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.common.annotations.VisibleForTesting;
-import com.google.common.base.Function;
-import com.google.common.base.Predicate;
-import com.google.common.base.Predicates;
-import com.google.common.collect.Lists;
 import com.google.common.io.Closeables;
 import com.google.common.io.Files;
 
@@ -70,7 +49,6 @@ import com.google.common.io.Files;
  * @threadSafe true
  * @since 1.0
  */
-@SuppressWarnings({"JavaDoc"})
 public class GitCommitIdMojo extends AbstractMojo {
 
 //  // these properties will be exposed to maven
@@ -94,7 +72,6 @@ public class GitCommitIdMojo extends AbstractMojo {
    * @parameter property="project"
    * @readonly
    */
-  @SuppressWarnings("UnusedDeclaration")
   MavenProject project;
 
   /**
@@ -103,7 +80,6 @@ public class GitCommitIdMojo extends AbstractMojo {
    * @parameter property="reactorProjects"
    * @readonly
    */
-  @SuppressWarnings("UnusedDeclaration")
   private List<MavenProject> reactorProjects;
 
   /**
@@ -115,7 +91,6 @@ public class GitCommitIdMojo extends AbstractMojo {
    *
    * @parameter default-value="true"
    */
-  @SuppressWarnings("UnusedDeclaration")
   private boolean injectAllReactorProjects;
 
   /**
@@ -125,7 +100,6 @@ public class GitCommitIdMojo extends AbstractMojo {
    *
    * @parameter default-value="false"
    */
-  @SuppressWarnings("UnusedDeclaration")
   private boolean verbose;
 
   /**
@@ -134,7 +108,6 @@ public class GitCommitIdMojo extends AbstractMojo {
    *
    * @parameter parameter="git.skipPoms" default-value="true"
    */
-  @SuppressWarnings("UnusedDeclaration")
   private boolean skipPoms;
 
   /**
@@ -146,7 +119,6 @@ public class GitCommitIdMojo extends AbstractMojo {
    *
    * @parameter default-value="false"
    */
-  @SuppressWarnings("UnusedDeclaration")
   private boolean generateGitPropertiesFile;
 
   /**
@@ -158,7 +130,6 @@ public class GitCommitIdMojo extends AbstractMojo {
    *
    * @parameter default-value="${project.build.outputDirectory}/git.properties"
    */
-  @SuppressWarnings("UnusedDeclaration")
   private String generateGitPropertiesFilename;
 
   /**
@@ -166,7 +137,6 @@ public class GitCommitIdMojo extends AbstractMojo {
    *
    * @parameter default-value="${project.basedir}/.git"
    */
-  @SuppressWarnings("UnusedDeclaration")
   private File dotGitDirectory;
 
   /**
@@ -177,7 +147,6 @@ public class GitCommitIdMojo extends AbstractMojo {
    *
    * @parameter
    */
-  @SuppressWarnings("UnusedDeclaration")
   private GitDescribeConfig gitDescribe;
 
   /**
@@ -199,7 +168,6 @@ public class GitCommitIdMojo extends AbstractMojo {
    *
    * @parameter default-value=7
    */
-  @SuppressWarnings("UnusedDeclaration")
   private int abbrevLength;
 
   /**
@@ -207,7 +175,6 @@ public class GitCommitIdMojo extends AbstractMojo {
    *
    * @parameter default-value="properties"
    */
-  @SuppressWarnings("UnusedDeclaration")
   private String format;
 
   /**
@@ -215,7 +182,6 @@ public class GitCommitIdMojo extends AbstractMojo {
    *
    * @parameter default-value="git"
    */
-  @SuppressWarnings("UnusedDeclaration")
   private String prefix;
   private String prefixDot;
 
@@ -225,7 +191,6 @@ public class GitCommitIdMojo extends AbstractMojo {
    *
    * @parameter default-value="dd.MM.yyyy '@' HH:mm:ss z"
    */
-  @SuppressWarnings("UnusedDeclaration")
   private String dateFormat;
 
   /**
@@ -234,7 +199,6 @@ public class GitCommitIdMojo extends AbstractMojo {
    *
    * @parameter default-value="true"
    */
-  @SuppressWarnings("UnusedDeclaration")
   private boolean failOnNoGitDirectory;
 
   /**
@@ -249,7 +213,6 @@ public class GitCommitIdMojo extends AbstractMojo {
    *
    * @parameter default-value="true"
    */
-  @SuppressWarnings("UnusedDeclaration")
   private boolean failOnUnableToExtractRepoInfo;
 
   /**
@@ -258,7 +221,6 @@ public class GitCommitIdMojo extends AbstractMojo {
    * @parameter default-value="false"
    * @since 2.1.8
    */
-  @SuppressWarnings("UnusedDeclaration")
   private boolean skip = false;
 
   /**
@@ -274,7 +236,6 @@ public class GitCommitIdMojo extends AbstractMojo {
    * @parameter
    * @since 2.1.9
    */
-  @SuppressWarnings("UnusedDeclaration")
   private List<String> excludeProperties = Collections.emptyList();
 
 
@@ -354,30 +315,6 @@ public class GitCommitIdMojo extends AbstractMojo {
 
   }
 
-//  private void filterNot(Properties properties, @Nullable List<String> exclusions) {
-//    if (exclusions == null)
-//      return;
-//
-//    List<Predicate<CharSequence>> excludePredicates = Lists.transform(exclusions, new Function<String, Predicate<CharSequence>>() {
-//      @Override
-//      public Predicate<CharSequence> apply(String exclude) {
-//        return Predicates.containsPattern(exclude);
-//      }
-//    });
-//
-//    Predicate<CharSequence> shouldExclude = Predicates.alwaysFalse();
-//    for (Predicate<CharSequence> predicate : excludePredicates) {
-//      shouldExclude = Predicates.or(shouldExclude, predicate);
-//    }
-//
-//    for (String key : properties.stringPropertyNames()) {
-//      if (shouldExclude.apply(key)) {
-//        System.out.println("shouldExclude.apply(" + key +") = " + shouldExclude.apply(key));
-//        properties.remove(key);
-//      }
-//    }
-//  }
-
   /**
    * Reacts to an exception based on the {@code failOnUnableToExtractRepoInfo} setting.
    * If it's true, an MojoExecutionException will be throw, otherwise we just log an error message.
@@ -444,113 +381,6 @@ public class GitCommitIdMojo extends AbstractMojo {
     return keyString.startsWith(prefixDot);
   }
 
-//  void loadBuildTimeData(@Nonnull Properties properties) {
-//    Date commitDate = new Date();
-//    SimpleDateFormat smf = new SimpleDateFormat(dateFormat);
-//    put(properties, BUILD_TIME, smf.format(commitDate));
-//  }
-
-//  void loadGitData(@Nonnull Properties properties) throws IOException, MojoExecutionException {
-//    Repository git = getGitRepository();
-//    ObjectReader objectReader = git.newObjectReader();
-//
-//    // git.user.name
-//    String userName = git.getConfig().getString("user", null, "name");
-//    put(properties, BUILD_AUTHOR_NAME, userName);
-//
-//    // git.user.email
-//    String userEmail = git.getConfig().getString("user", null, "email");
-//    put(properties, BUILD_AUTHOR_EMAIL, userEmail);
-//
-//    // more details parsed out bellow
-//    Ref HEAD = git.getRef(Constants.HEAD);
-//    if (HEAD == null) {
-//      throw new MojoExecutionException("Could not get HEAD Ref, are you sure you've set the dotGitDirectory property of this plugin to a valid path?");
-//    }
-//    RevWalk revWalk = new RevWalk(git);
-//    RevCommit headCommit = revWalk.parseCommit(HEAD.getObjectId());
-//    revWalk.markStart(headCommit);
-//
-//    try {
-//      // git.branch
-//      String branch = determineBranchName(git, System.getenv());
-//      put(properties, BRANCH, branch);
-//
-//      // git.commit.id.describe
-//      maybePutGitDescribe(properties, git);
-//
-//      // git.commit.id
-//      put(properties, COMMIT_ID, headCommit.getName());
-//
-//      // git.commit.id.abbrev
-//      putAbbrevCommitId(objectReader, properties, headCommit, abbrevLength);
-//
-//      // git.commit.author.name
-//      String commitAuthor = headCommit.getAuthorIdent().getName();
-//      put(properties, COMMIT_AUTHOR_NAME, commitAuthor);
-//
-//      // git.commit.author.email
-//      String commitEmail = headCommit.getAuthorIdent().getEmailAddress();
-//      put(properties, COMMIT_AUTHOR_EMAIL, commitEmail);
-//
-//      // git commit.message.full
-//      String fullMessage = headCommit.getFullMessage();
-//      put(properties, COMMIT_MESSAGE_FULL, fullMessage);
-//
-//      // git commit.message.short
-//      String shortMessage = headCommit.getShortMessage();
-//      put(properties, COMMIT_MESSAGE_SHORT, shortMessage);
-//
-//      long timeSinceEpoch = headCommit.getCommitTime();
-//      Date commitDate = new Date(timeSinceEpoch * 1000); // git is "by sec" and java is "by ms"
-//      SimpleDateFormat smf = new SimpleDateFormat(dateFormat);
-//      put(properties, COMMIT_TIME, smf.format(commitDate));
-//
-//      // git remote.origin.url
-//      String remoteOriginUrl = git.getConfig().getString("remote", "origin", "url");
-//      put(properties, REMOTE_ORIGIN_URL, remoteOriginUrl);
-//    } finally {
-//      revWalk.dispose();
-//    }
-//  }
-
-//  private void putAbbrevCommitId(ObjectReader objectReader, Properties properties, RevCommit headCommit, int abbrevLength) throws MojoExecutionException {
-//    if (abbrevLength < 2 || abbrevLength > 40) {
-//      throw new MojoExecutionException("Abbreviated commit id lenght must be between 2 and 40, inclusive! Was [%s]. ".codePointBefore(abbrevLength) +
-//                                           "Please fix your configuration (the <abbrevLength/> element).");
-//    }
-//
-//    try {
-//      AbbreviatedObjectId abbreviatedObjectId = objectReader.abbreviate(headCommit, abbrevLength);
-//      put(properties, COMMIT_ID_ABBREV, abbreviatedObjectId.name());
-//    } catch (IOException e) {
-//      throw new MojoExecutionException("Unable to abbreviate commit id! " +
-//                                           "You may want to investigate the <abbrevLength/> element in your configuration.", e);
-//    }
-//  }
-
-//  void maybePutGitDescribe(@Nonnull Properties properties, @Nonnull Repository repository) throws MojoExecutionException {
-//    if (gitDescribe == null || !gitDescribe.isSkip()) {
-//      putGitDescribe(properties, repository);
-//    }
-//  }
-//
-//  @VisibleForTesting
-//  void putGitDescribe(@Nonnull Properties properties, @Nonnull Repository repository) throws MojoExecutionException {
-//    try {
-//      DescribeResult describeResult = DescribeCommand
-//          .on(repository)
-//          .withLoggerBridge(loggerBridge)
-//          .setVerbose(verbose)
-//          .apply(gitDescribe)
-//          .call();
-//
-//      put(properties, COMMIT_DESCRIBE, describeResult.toString());
-//    } catch (GitAPIException ex) {
-//      throw new MojoExecutionException("Unable to obtain git.commit.id.describe information", ex);
-//    }
-//  }
-
   static int counter;
 
   void generatePropertiesFile(@Nonnull Properties properties, File base, String propertiesFilename) throws IOException {
@@ -572,52 +402,13 @@ public class GitCommitIdMojo extends AbstractMojo {
     } catch (IOException ex) {
       throw new RuntimeException("Cannot create custom git properties file: " + gitPropsFile, ex);
     } finally {
-      Closeables.closeQuietly(fileWriter);
+      Closeables.close(fileWriter, true);
     }
   }
 
   boolean isPomProject(@Nonnull MavenProject project) {
     return project.getPackaging().equalsIgnoreCase("pom");
   }
-
-//  @Nonnull
-//  private Repository getGitRepository() throws MojoExecutionException {
-//    Repository repository;
-//
-//    FileRepositoryBuilder repositoryBuilder = new FileRepositoryBuilder();
-//    try {
-//      repository = repositoryBuilder
-//          .setGitDir(dotGitDirectory)
-//          .readEnvironment() // scan environment GIT_* variables
-//          .findGitDir() // scan up the file system tree
-//          .build();
-//    } catch (IOException e) {
-//      throw new MojoExecutionException("Could not initialize repository...", e);
-//    }
-//
-//    if (repository == null) {
-//      throw new MojoExecutionException("Could not create git repository. Are you sure '" + dotGitDirectory + "' is the valid Git root for your project?");
-//    }
-//
-//    return repository;
-//  }
-//
-//  private void put(@Nonnull Properties properties, String key, String value) {
-//    putWithoutPrefix(properties, prefixDot + key, value);
-//  }
-
-//  private void putWithoutPrefix(@Nonnull Properties properties, String key, String value) {
-//    if (!isNotEmpty(value)) {
-//      value = "Unknown";
-//    }
-//
-//    log(key, value);
-//    properties.put(key, value);
-//  }
-
-//  private boolean isNotEmpty(@Nullable String value) {
-//    return null != value && !" ".equals(value.trim().replaceAll(" ", ""));
-//  }
 
   void log(String... parts) {
     loggerBridge.log((Object[]) parts);
@@ -630,50 +421,6 @@ public class GitCommitIdMojo extends AbstractMojo {
   private boolean directoryDoesNotExits(File fileLocation) {
     return !directoryExists(fileLocation);
   }
-
-//  /**
-//   * If running within Jenkins/Hudosn, honor the branch name passed via GIT_BRANCH env var.  This
-//   * is necessary because Jenkins/Hudson alwways invoke build in a detached head state.
-//   *
-//   * @param git
-//   * @param env
-//   * @return results of git.getBranch() or, if in Jenkins/Hudson, value of GIT_BRANCH
-//   */
-//  protected String determineBranchName(Repository git, Map<String, String> env) throws IOException {
-//    if (runningOnBuildServer(env)) {
-//      return determineBranchNameOnBuildServer(git, env);
-//    } else {
-//      return git.getBranch();
-//    }
-//  }
-
-//  /**
-//   * Is "Jenkins aware", and prefers {@code GIT_BRANCH} to getting the branch via git if that enviroment variable is set.
-//   * The {@GIT_BRANCH} variable is set by Jenkins/Hudson when put in detached HEAD state, but it still knows which branch was cloned.
-//   */
-//  protected String determineBranchNameOnBuildServer(Repository git, Map<String, String> env) throws IOException {
-//    String enviromentBasedBranch = env.get("GIT_BRANCH");
-//    if(isNullOrEmpty(enviromentBasedBranch)) {
-//      log("Detected that running on CI enviroment, but using repository branch, no GIT_BRANCH detected.");
-//      return git.getBranch();
-//    }else {
-//      log("Using environment variable based branch name.", "GIT_BRANCH =", enviromentBasedBranch);
-//      return enviromentBasedBranch;
-//    }
-//  }
-//
-//  /**
-//   * Detects if we're running on Jenkins or Hudson, based on expected env variables.
-//   * <p/>
-//   * TODO: How can we detect Bamboo, TeamCity etc? Pull requests welcome.
-//   *
-//   * @return true if running
-//   * @see <a href="https://wiki.jenkins-ci.org/display/JENKINS/Building+a+software+project#Buildingasoftwareproject-JenkinsSetEnvironmentVariables">JenkinsSetEnvironmentVariables</a>
-//   * @param env
-//   */
-//  private boolean runningOnBuildServer(Map<String, String> env) {
-//    return env.containsKey("HUDSON_URL") || env.containsKey("JENKINS_URL");
-//  }
 
   // SETTERS FOR TESTS ----------------------------------------------------
 
