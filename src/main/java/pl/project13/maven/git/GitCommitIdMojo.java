12c8a52211b5ffbaa8e7fc4e5eca432548ca2223
Derrick Burns <derrickrburns@gmail.com>
2014-06-07 15:03:17 -0700

    Update GitCommitIdMojo.java
    Changed key name for COMMIT_ID to commit.id.full

--- src/main/java/pl/project13/maven/git/GitCommitIdMojo.java
+++ src/main/java/pl/project13/maven/git/GitCommitIdMojo.java
@@ -58,7 +58,7 @@ public class GitCommitIdMojo extends AbstractMojo {
 
   // these properties will be exposed to maven
   public static final String BRANCH = "branch";
-  public static final String COMMIT_ID = "commit.id";
+  public static final String COMMIT_ID = "commit.id.full";
   public static final String COMMIT_ID_ABBREV = "commit.id.abbrev";
   public static final String COMMIT_DESCRIBE = "commit.id.describe";
   public static final String COMMIT_SHORT_DESCRIBE = "commit.id.describe-short";
