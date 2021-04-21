587254040530ec0dd55cffb8b0f3b0e38ec1d505
Matteo Baccan <matteo.baccan@gmail.com>
2021-04-21 02:28:03 +0200

    Return of boolean expression should not be wrapped into an if-the-else statement Signed-off-by:Matteo Baccan <matteo.baccan@gmail.com>

--- core/src/main/java/pl/project13/core/log/MessageFormatter.java
+++ core/src/main/java/pl/project13/core/log/MessageFormatter.java
@@ -261,19 +261,11 @@ public final class MessageFormatter {
       return false;
     }
     char potentialEscape = messagePattern.charAt(delimeterStartIndex - 1);
-    if (potentialEscape == ESCAPE_CHAR) {
-      return true;
-    } else {
-      return false;
-    }
+    return (potentialEscape == ESCAPE_CHAR);
   }
 
   static boolean isDoubleEscaped(String messagePattern, int delimeterStartIndex) {
-    if (delimeterStartIndex >= 2 && messagePattern.charAt(delimeterStartIndex - 2) == ESCAPE_CHAR) {
-      return true;
-    } else {
-      return false;
-    }
+    return (delimeterStartIndex >= 2 && messagePattern.charAt(delimeterStartIndex - 2) == ESCAPE_CHAR);
   }
 
   // special treatment of array values was suggested by 'lizongbo'
