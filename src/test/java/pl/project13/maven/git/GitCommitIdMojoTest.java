88fae5168800fbad18882be5323224b0ce4ed2ca
TheSnoozer <s.lange@alumni.tu-berlin.de>
2024-02-24 19:28:12 +0100

    attempt to make tests work

--- src/test/java/pl/project13/maven/git/GitCommitIdMojoTest.java
+++ src/test/java/pl/project13/maven/git/GitCommitIdMojoTest.java
@@ -22,10 +22,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.io.File;
 import java.io.IOException;
-import java.time.Instant;
 import java.util.Date;
+
 import junitparams.JUnitParamsRunner;
 import junitparams.Parameters;
+import org.joda.time.DateTime;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import pl.project13.core.PropertiesFileGenerator;
@@ -74,135 +75,75 @@ public class GitCommitIdMojoTest {
   }
 
   private Object[] parametersParseOutputTimestamp() {
+    /**
+     * test cases for output timestamp parsing.
+     * This timestamp is configured for Reproducible Builds' archive entries
+     * (https://maven.apache.org/guides/mini/guide-reproducible-builds.html). The value from <code>
+     * ${project.build.outputTimestamp}</code> is either formatted as ISO 8601 <code>
+     * yyyy-MM-dd'T'HH:mm:ssXXX</code> or as an int representing seconds since the epoch (like <a
+     * href="https://reproducible-builds.org/docs/source-date-epoch/">SOURCE_DATE_EPOCH</a>.
+     * When using ISO 8601 formatting please note that the entire expression must be entirely either
+     * in the basic format (20240215T135459+0100) or in the
+     * extended format (e.g. 2024-02-15T13:54:59+01:00).
+     * The maven plugin only supports the extended format.
+     */
     return new Object[] {
       // long since epoch
       new Object[] {
         "1644689403",
-        Instant.ofEpochMilli(1644689403000L)
-      },
-      // Date only:
-      new Object[] {
-        "2022-02",
-        Instant.ofEpochMilli(1643670000000L)
-      },
-      new Object[] {
-        "2022-02-12",
-        Instant.ofEpochMilli(1644620400000L)
-      },
-      // Date and time:
-      new Object[] {
-        "2022-02-12T15:30",
-        Instant.ofEpochMilli(1644676200000L)
-      },
-      new Object[] {
-        "2022-02-12T15:30:45",
-        Instant.ofEpochMilli(1644676245000L)
+        new DateTime("2022-02-12T19:10:03").toDate()
       },
       // Date and time with timezone:
       new Object[] {
         "2022-02-12T15:30+00:00",
-        Instant.ofEpochMilli(1644679800000L)
+        new DateTime("2022-02-12T15:30:00+00:00").toDate()
       },
       new Object[] {
         "2022-02-12T15:30:45-05:00",
-        Instant.ofEpochMilli(1644697845000L)
+        new DateTime("2022-02-12T15:30:45-05:00").toDate()
       },
       new Object[] {
         "2022-02-12T15:30:00+00:00",
-        Instant.ofEpochMilli(1644679800000L)
+        new DateTime("2022-02-12T15:30:00+00:00").toDate()
       },
       new Object[] {
         "2023-11-30T09:17:06+05:30",
-        Instant.ofEpochMilli(1701316026000L)
+        new DateTime("2023-11-30T09:17:06+05:30").toDate()
       },
       new Object[] {
         "2024-08-15T20:45:30-03:00",
-        Instant.ofEpochMilli(1723765530000L)
+        new DateTime("2024-08-15T20:45:30-03:00").toDate()
       },
       new Object[] {
         "2022-02-12T15:30:00Z",
-        Instant.ofEpochMilli(1644679800000L)
+        new DateTime("2022-02-12T15:30:00Z").toDate()
       },
-      // Not valid according to the ISO 8601 standard. The issue is with the time zone
-      // representation. ISO 8601 uses a specific format for time zones, either as "Z" for UTC or
-      // in the format "+HH:MM" or "-HH:MM" for the offset from UTC.
-      // The time zone "EST" or "PST" does not follow this format.
-      // new Object[] { "2023-11-30T09:17:06PST", null },
-      // new Object[] { "2024-08-15T20:45:30EST", null },
       new Object[] {
         "2023-11-30T09:17:06+0100",
-        Instant.ofEpochMilli(1701332226000L)
-      },
-      // Week date:
-      new Object[] {
-        "2022-W06",
-        Instant.ofEpochMilli(1644188400000L)
-      },
-      new Object[] {
-        "2022-W06-5",
-        Instant.ofEpochMilli(1644534000000L)
-      },
-      // Week date with time:
-      new Object[] {
-        "2022-W06-5T15:30",
-        Instant.ofEpochMilli(1644589800000L)
-      },
-      new Object[] {
-        "2022-W06-5T15:30:45",
-        Instant.ofEpochMilli(1644589845000L)
-      },
-      // https://tc39.es/proposal-uniform-interchange-date-parsing/cases.html
-      // positive leap second
-      // not working: new Object[] { "1972-06-30T23:59:60Z", null },
-      // Too few fractional second digits
-      new Object[] {
-        "2019-03-26T14:00:00.9Z",
-        Instant.ofEpochMilli(1553608800900L)
-      },
-      // Too many fractional second digits
-      new Object[] {
-        "2019-03-26T14:00:00.4999Z",
-        Instant.ofEpochMilli(1553608800499L)
-      },
-      // Too many fractional second digits (pre-epoch)
-      new Object[] {
-        "1969-03-26T14:00:00.4999Z",
-        Instant.ofEpochMilli(-24227999501L)
-      },
-      // Too many fractional second digits (BCE)
-      new Object[] {
-        "-000043-03-15T14:00:00.4999Z",
-        Instant.ofEpochMilli(-63517773599501L)
+        new DateTime("2023-11-30T09:17:06+01:00").toDate()
       },
       // Lowercase time designator
       new Object[] {
         "2019-03-26t14:00Z",
-        Instant.ofEpochMilli(1553608800000L)
+        new DateTime("2019-03-26T14:00Z").toDate()
       },
       // Lowercase UTC designator
       new Object[] {
         "2019-03-26T14:00z",
-        Instant.ofEpochMilli(1553608800000L)
+        new DateTime("2019-03-26T14:00:00Z").toDate()
       },
       // Hours-only offset
       new Object[] {
         "2019-03-26T10:00-04",
-        Instant.ofEpochMilli(1553608800000L)
+        new DateTime("2019-03-26T10:00-04:00").toDate()
       },
-      // Fractional minutes
-      new Object[] {
-        "2019-03-26T14:00.9Z",
-        Instant.ofEpochMilli(1553608854000L)
-      },
-      // ISO basic format date and time
-      // not working: new Object[] { "20190326T1400Z", null },
     };
   }
 
   @Test
   @Parameters(method = "parametersParseOutputTimestamp")
-  public void testParseOutputTimestamp(String input, Instant expected) {
+  public void testParseOutputTimestamp(String input, Date expected) {
     Date actual = GitCommitIdMojo.parseOutputTimestamp(input);
-    assertThat(actual.toInstant()).isEqualTo(expected);
+    assertThat(actual).isEqualTo(expected);
   }
 }
